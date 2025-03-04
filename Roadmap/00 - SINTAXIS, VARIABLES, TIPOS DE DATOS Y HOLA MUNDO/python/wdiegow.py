e8d6039bc85b51edd7bb0f435c202cd1bcada7fb
diego <diegoastetepaz@gmail.com>
2025-03-04 16:09:30 -0400

    #00 - Python

---
+++ Roadmap/00 - SINTAXIS, VARIABLES, TIPOS DE DATOS Y HOLA MUNDO/python/wdiegow.py
@@ -0,0 +1,22 @@
+# https://www.python.org/
+
+# con el "#" se puede hacer comentarios de una sola linea y para comentarios de
+# varias lineas solo se puede usar el simbolo # ya que no tiene uno espesifico 
+#para los comentarios de varias lineas aunque se puede usar las comillas 
+"""
+Con las 3 comillas (dobles o simples) arriba y abajo se puede utilizar como para 
+hacer  comentarios aunque no es tecnicamente un comentario, mientras la cadena de 
+texto no sea asignada python lo ignorara 
+"""
+
+mi_variable = 1 # creando la inicializacion de una variable 
+MI_CONSTANTE = 2 # como python no tiene un dato espesifico para las constantes 
+# entonces se las define usando mayusculas
+
+mi_entero = 1
+mi_flotante = 3.1416
+mi_booleano = True
+mi_string = 'hola'
+mi_nada = None 
+
+print("hola, python")
